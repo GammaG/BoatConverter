b8173b7a283ba1281bbd245479d3222f1f32eb5e
mseidler <marco.seidler@outlook.com>
2016-07-27 14:36:54 +0200

    back to the roots

--- src/de/init/boatconverter/reader/ExcelReader.java
+++ src/de/init/boatconverter/reader/ExcelReader.java
@@ -33,7 +33,7 @@ public class ExcelReader {
 		// Create Workbook instance holding reference to .xlsx file
 		XSSFWorkbook workbook = new XSSFWorkbook(file);
 
-		for (int i = 1; i < workbook.getNumberOfSheets(); i++) {
+		for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
 			ArrayList<Object> valueList = new ArrayList<>();
 			// Get first/desired sheet from the workbook
 			XSSFSheet sheet = workbook.getSheetAt(i);
